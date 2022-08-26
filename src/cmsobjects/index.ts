e8ce6c6a14d5aded71dd1ef5384f8f617eed4ab8
Jonahan Klinger <jo@cfmmedia.de>
2022-08-26 23:10:05 +0200

    add Section pbject

--- src/cmsobjects/index.ts
+++ src/cmsobjects/index.ts
@@ -1,9 +1,11 @@
 export const CmsObjectTypes = Object.freeze({
     Headline: "Headline",
     Text: "Text",
+    Section: "Section",
 })
 
 export default {
     Headline: require("./headline/Headline.js"),
     Text: require("./text/Text.js"),
+    Section: require("./text/Section.js"),
 }
---
+++ src/cmsobjects/section/Section.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+type settingsType = {
+    color: any,
+    marginTop: number
+    marginBottom: number
+}
+
+const defaultSettings: settingsType = {
+    color: "rgba(0,0,0,0)",
+    marginTop: 20,
+    marginBottom: 20,
+}
+
+const Section = props => {
+
+    const {
+        settings: _settings,
+        id,
+        children
+    } = props;
+
+    const settings = {...defaultSettings, _settings};
+
+    return (
+        <>
+            <div style={{
+                color: settings.color,
+                marginTop: settings.marginTop + "px",
+                marginBottom: settings.marginBottom + "px",
+            }}>
+                <div style={{maxWidth: "900px", margin: "auto"}}>
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Section;
